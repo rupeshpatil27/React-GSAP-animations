3e3f9b196f3f38f84116b293e5982869b82ec0d0
rupeshpatil <rupeshpatil88927@gmail.com>
2025-05-23 18:02:13 +0530

    typewriter animation GSAP

--- text-animation/src/App.jsx
+++ text-animation/src/App.jsx
@@ -3,8 +3,12 @@ import "./App.css";
 
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
+import { TextPlugin } from "gsap/all";
 
 function App() {
+
+  gsap.registerPlugin(TextPlugin);
+
   useGSAP(() => {
     gsap.fromTo(
       ".motion-txt-right-left h1",
@@ -40,16 +44,63 @@ function App() {
       }
     );
 
-    var tl = gsap.timeline({repeat:-1});
+  
+    var tl = gsap.timeline({ repeat: -1 });
+    tl.from(".fading-txt h1", { duration: 1, x: 100, opacity: 0.5, repeatDelay: 1, ease: "bounce" });
 
-    tl.from(".fading-txt h1", { duration: 1, x: 100, opacity: 0.5 ,repeatDelay:1,ease:"bounce"});
 
+    const t2 = gsap.timeline({ onStart: () => blinking.play(), repeat: -1 });
+
+    t2.from(".typewriter-txt h1:nth-child(1)", {
+      text: {
+        value: "",
+        delimiter: ""
+      },
+      duration: 2,
+      ease: "none"
+    });
+    t2.from(".typewriter-txt h1:nth-child(2)", {
+      text: {
+        value: "",
+        delimiter: ""
+      },
+      duration: 2,
+      ease: "none"
+    });
+
+    t2.from(".typewriter-txt h1:nth-child(3)", {
+      text: {
+        value: "",
+        delimiter: ""
+      },
+      duration: 2,
+      ease: "none"
+    });
+
+
+    // gsap.to("#cursor", {
+    //   opacity: 0,
+    //   duration: 0.6,
+    //   ease: "power2.inOut",
+    //   repeat: -1,
+    //   yoyo: true
+    // });
+
+    // const blinking = gsap.timeline({
+    //   repeat: -1,
+    //   paused: true
+    // });
+
+    // blinking.from("#cursor", {
+    //   opacity: -100,
+    //   ease: "steps(1)"
+    // });
 
   });
 
   return (
     <>
- 
+
       <div className="grid grid-cols-2 gap-4">
         <div className="motion-txt-right-left flex-col flex-center">
           <h1>Welcome!</h1>
@@ -62,14 +113,19 @@ function App() {
           <h1>This is motion text animation</h1>
           <h1>Good luck!</h1>
         </div>
-        
+
         <div className="fading-txt flex-col flex-center">
           <h1>Welcome!</h1>
           <h1>This is motion text animation</h1>
           <h1>Good luck!</h1>
         </div>
 
-     
+        <div className="typewriter-txt flex-col flex-center">
+          <h1>Welcome!</h1>
+          <h1>This is motion text animation</h1>
+          <h1>Good luck!</h1>
+          {/* <span id="cursor">|</span> */}
+        </div>
       </div>
     </>
   );
