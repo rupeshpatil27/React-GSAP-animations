6fafbaf7b4c6dcab7a2c85bb4dfdc85f2df6cd9f
rupeshpatil <rupeshpatil88927@gmail.com>
2025-05-23 16:34:59 +0530

    bounce animation GSAP

--- text-animation/src/App.jsx
+++ text-animation/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import "./App.css";
 
 import { useGSAP } from "@gsap/react";
@@ -7,7 +7,7 @@ import gsap from "gsap";
 function App() {
   useGSAP(() => {
     gsap.fromTo(
-      ".motion-txt h1:nth-child(1),.motion-txt h1:nth-child(3)",
+      ".motion-txt-right-left h1",
       {
         x: 50,
         opacity: 0,
@@ -17,52 +17,59 @@ function App() {
         opacity: 1,
         stagger: 0.2,
         duration: 1,
-        ease: 'power2.inOut'
+        repeat: -1,
+        repeatDelay: 0.5,
+        ease: "power2.inOut",
       }
     );
+
     gsap.fromTo(
-      ".motion-txt h1:nth-child(2)",
+      ".motion-txt-bottom-top h1",
       {
-        x: -50,
+        y: 50,
         opacity: 0,
       },
       {
-        x: 0,
+        y: 0,
         opacity: 1,
         stagger: 0.2,
         duration: 1,
-        ease: 'power2.inOut',
+        repeat: -1,
+        repeatDelay: 0.5,
+        ease: "power2.inOut",
       }
     );
+
+    var tl = gsap.timeline({repeat:-1});
+
+    tl.from(".fading-txt h1", { duration: 1, x: 100, opacity: 0.5 ,repeatDelay:1,ease:"bounce"});
+
+
   });
 
   return (
     <>
-      {/* <div className="h-100 flex-col flex-center">
-        <h1>This is demo text</h1>
-        <h1>Practice Animation</h1>
-        <h1>Good luck!</h1>
-      </div> */}
-
+ 
       <div className="grid grid-cols-2 gap-4">
-        <div className="motion-txt flex-col flex-center">
-
-          {/* <span className="red-200">motion from left right</span> */}
-
+        <div className="motion-txt-right-left flex-col flex-center">
           <h1>Welcome!</h1>
           <h1>This is motion text animation</h1>
           <h1>Good luck!</h1>
         </div>
-        <div className="typewriter-txt flex-col flex-center">
+
+        <div className="motion-txt-bottom-top flex-col flex-center">
           <h1>Welcome!</h1>
           <h1>This is motion text animation</h1>
           <h1>Good luck!</h1>
         </div>
-        {/* <div className="motion-txt flex-col flex-center">
+        
+        <div className="fading-txt flex-col flex-center">
           <h1>Welcome!</h1>
           <h1>This is motion text animation</h1>
           <h1>Good luck!</h1>
-        </div> */}
+        </div>
+
+     
       </div>
     </>
   );
